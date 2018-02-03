ab354b274dc269caf2868de5c3f357e33e7074b8
Nick "Николай" Pittman <astronomer346@gmail.com>
2018-02-03 14:44:21 -0500

    Added basic framework for Vision subsystem

--- src/main/java/frc/team166/robot/subsystems/Vision.java
+++ src/main/java/frc/team166/robot/subsystems/Vision.java
@@ -7,9 +7,12 @@
 
 package frc.team166.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.team166.chopshoplib.commands.SubsystemCommand;
+
+import java.util.Set;;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
@@ -17,11 +20,40 @@ import edu.wpi.first.networktables.NetworkTableInstance;;
 public class Vision extends Subsystem {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("/GRIP/VisionTarget");
 
+    public Vision() {
+        addChild(getTableKeys());
+    }
+
+    public Set<String> getTableKeys() {
+        return table.getKeys();
+    }
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new SubsystemCommand(this) {
+
+            @Override
+            protected void initialize() {
+
+            }
+
+            @Override
+            protected void execute() {
+
+            }
+
+            @Override
+            protected boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            protected void end() {
+
+            }
+        });
     }
 }
